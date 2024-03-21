edac42958af2e9d0bc1e78c1a4eac2e5b75c9f2d
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-03-21 23:30:15 +0530

    added mysql driver  dependency in pom.xml file and created user model class and also connected to database

--- src/main/java/examifyserver/ExamifyServerApplication.java
+++ src/main/java/examifyserver/ExamifyServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExamifyServerApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(ExamifyServerApplication.class, args);
+		SpringApplication.run(ExamifyServerApplication.class, args);
 		System.out.println("hello exam portal see here ");
 	}
 
---
+++ src/main/java/examifyserver/model/User.java
@@ -0,0 +1,11 @@
+/*
+ * Author Name:
+ * Date: 3/21/2024
+ * Created With: IntelliJ IDEA Community Edition
+ */
+
+
+package examifyserver.model;
+
+public class User {
+}
