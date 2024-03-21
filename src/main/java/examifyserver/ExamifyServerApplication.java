1c6caa4d5cec7962d5677f43070932851f78bc5a
Sanjay Singh Rawat <rawatsanjay2799@gmail.com>
2024-03-21 22:46:03 +0530

    first commit

---
+++ src/main/java/examifyserver/ExamifyServerApplication.java
@@ -0,0 +1,14 @@
+package examifyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamifyServerApplication {
+
+	public static void main(String[] args) {
+//		SpringApplication.run(ExamifyServerApplication.class, args);
+		System.out.println("hello exam portal see here ");
+	}
+
+}
---
+++ src/test/java/examifyserver/ExamifyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package examifyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamifyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
